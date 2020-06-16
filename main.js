00f5c09a963c4b3dac6f36505895059eab6220ed
Garrett4 <gmcox712@gmail.com>
2020-06-16 14:32:36 -0500

    clean

--- main.js
+++ main.js
@@ -22,17 +22,3 @@ linkElement.append(linkName)
 linkElement.title = "Link to Baby Yoda"
 linkElement.href = "https://www.firstcoastnews.com/article/entertainment/a-look-back-at-the-top-5-memes-of-2019/77-e945bfd5-5033-429e-9526-3c19a954a699"
 main.append(linkElement)
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 
